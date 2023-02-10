f58794e69bdc3c8ff42b9e6ba18e39510aa9bfbc
DanishRayeen <danishrayeen32@gmail.com>
2023-02-10 20:06:55 +0530

    JavaScript

---
+++ Variable.js
@@ -0,0 +1,44 @@
+// We can declare variables to store data
+// let – is a modern variable declaration.
+// var – is an old-school variable declaration, Normally we don’t use it.
+// const – is like let, but the value of the variable can’t be changed.
+// once you declare const value you cannot change it.
+
+
+// integer
+
+let inte = 15;
+console.log("---------------------------")
+console.log("it is a number/integer vlaue:", inte)
+console.log("---------------------------")
+console.log(typeof(inte));
+
+// String
+
+let str = "ineuron.ai";
+console.log("---------------------------")
+console.log("it is a string value:", str);
+console.log("---------------------------")
+console.log(typeof(str));
+
+// Undefined
+
+let und ;
+console.log("---------------------------")
+console.log("it is a undefined vlaue:", und);
+console.log("---------------------------")
+console.log(typeof(und));
+
+// Boolean
+
+let bool = true;
+console.log("---------------------------")
+console.log("it is a boolean value", bool);
+console.log("---------------------------")
+console.log(typeof(bool));
+
+// Null
+let nul = null;
+console.log(nul);
+console.log(typeof(nul));
+
