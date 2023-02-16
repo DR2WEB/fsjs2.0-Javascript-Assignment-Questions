0b5b2b27f928864cd5132474c4ac3699532086b3
DanishRayeen <danishrayeen32@gmail.com>
2023-02-16 09:34:40 +0530

    javascript

---
+++ Question_08/trim.js
@@ -0,0 +1,6 @@
+// trim() method to remove whitespaces
+
+
+let str = "           ineuron.ai            ";
+console.log(str);
+console.log(str.trim());
---
+++ Question_09/bool.js
@@ -0,0 +1,6 @@
+let i = 0;
+
+if(i <= 10){
+    i++ ;
+    console.log("i is lessthan 10",i);
+}
